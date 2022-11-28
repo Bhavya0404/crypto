7c8c2dcfabf98370ec752b1b7e4d97eb195e4a2f
Meenal Prakash <meenalprakash03@gmail.com>
2022-11-28 22:21:49 +0530

    gg

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -15,56 +15,25 @@ import { CryptoState } from "../CryptoContext";
 import HTMLReactParser from "html-react-parser";
 import { ListCoins } from "../utils/apiService";
 import { MonthlyCoin } from "../utils/apiService";
+import { useCallback } from "react";
 
 const CoinPage = () => {
-  const [low, setLow] = React.useState(0);
-  const [high, setHigh] = React.useState(0);
-  const [close, setClose] = React.useState(0);
+ // const [low, setLow] = React.useState(0);
+ // const [high, setHigh] = React.useState(0);
+//  const [close, setClose] = React.useState(0);
 
   const [resistance1, setResistance1] = React.useState(0);
   const [resistance2, setResistance2] = React.useState(0);
   const [support1, setSupport1] = React.useState(0);
   const [support2, setSupport2] = React.useState(0);
   const [pivotPoint, setPivotPoint] = React.useState(0);
+  const [buy,setbuy] = React.useState("DON'T BUY");
 
   const { id } = useParams();
   const [coin, setCoin] = React.useState({});
   const [prevCoin, setPrevCoin] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const { currency, symbol } = CryptoState();
-
-  const fetchCoinData = async () => {
-    const { data } = await axios.get(CoinData(id));
-    // console.log(data.market_data.low_24h.inr)
-    // console.log(data.market_data.high_24h.inr)
-    // console.log(data.market_data.current_price.inr)
-    setLow(data.market_data.low_24h.inr)
-    setHigh(data.market_data.high_24h.inr)
-    setClose(data.market_data.current_price.inr)
-   // console.log(low)
-   // console.log(high)
-   // console.log(close)
-
-    setCoin(data);
-    setIsLoading(false);
-  };
-
- // console.log(coin.symbol)
-
-  const fetchPrevCoinData = async () => {
-    const ex1 = coin.symbol?.toString() || ''
-    const MonthlyData = await axios.get(MonthlyCoin(ex1));
-   // console.log(MonthlyData)
-    const neww = MonthlyData.data["Time Series (Digital Currency Weekly)"];
-   // console.log(neww["2020-03-15"])
-    for(var key in neww){
-      console.log(neww[key]) //open  close ke liye ["1a. open (INR)"] ye kr dena key ke aage
-    }
-    setPrevCoin(neww);
-    // setIsLoading(false);
-  }
-
-  
 
   let theme = createTheme();
   theme = responsiveFontSizes(theme);
@@ -122,7 +91,53 @@ const CoinPage = () => {
     },
   };
 
-  const Pivot = () => {
+  const fetchCoinData = async () => {
+    const { data } = await axios.get(CoinData(id));
+    // console.log(data.market_data.low_24h.inr)
+    // console.log(data.market_data.high_24h.inr)
+    // console.log(data.market_data.current_price.inr)
+   // setLow(data.market_data.low_24h.inr)
+   // setHigh(data.market_data.high_24h.inr)
+   // setClose(data.market_data.current_price.inr)
+   // console.log(low)
+   // console.log(high)
+   // console.log(close)
+
+    setCoin(data);
+    setIsLoading(false);
+  };
+
+  const month  = {low: "", high: "", close: ""};
+  const points = {r1: "", r2: "", pp: "", s1: "", s2: ""};
+ 
+  const fetchPrevCoinData = useCallback(async () => {
+    const ex1 = coin.symbol?.toString() || '';
+    console.log(ex1);
+    const MonthlyData = await axios.get(MonthlyCoin(ex1));
+   
+    console.log(MonthlyData)
+    const neww = MonthlyData.data["Time Series (Digital Currency Weekly)"];
+   // console.log(neww["2020-03-15"])
+   
+    for(var key in neww){
+      if(key == "2022-11-20"){
+        month.low = neww[key]["3a. low (INR)"];
+        month.high = neww[key]["2a. high (INR)"];
+        month.close = neww[key]["4a. close (INR)"];
+       // console.log(month.low)
+      //  console.log(month.high)
+      //  console.log(month.close)
+      }
+    }
+   // setPrevCoin(neww);
+    // setIsLoading(false);
+  
+  
+    console.log(month);
+  
+   let high=parseInt(month.high)
+   let low=parseInt(month.low)
+   let close=parseInt(month.close)
     let pp = (high + low + close) / 3;
    
     let r3 = pp + 2* (high - low)
@@ -131,31 +146,40 @@ const CoinPage = () => {
     
     let s1 = 2* pp - high;
     let s2 = pp - (high - low)
-    let s3 = pp - 2* (high - low)
-    
- // console.log("Range: ")
-
-  setResistance1(r1);
-  setResistance2(r2);
-  setPivotPoint(pp);
-  setSupport1(s1);
-  setSupport2(s2);
- //  console.log(resistance1);
- //  console.log(resistance2);
+    let s3 = pp - 2* (high - low)  
  
- //  console.log(pivotPoint);
- //  console.log(support1);
- //  console.log(support2);
+    points.r1 = r1;
+    points.r2 = r2;
+    points.pp = pp;
+    points.s1 = s1;
+    points.s2 = s2;
+    setResistance1(points.r1);
+    setResistance2(points.r2);
+    setPivotPoint(points.pp);
+    setSupport1(points.s1);
+    setSupport2(points.s2);
+   
 
 
-  };
+  
+});
+
+
+  console.log(points)
+  
+ const buynotbuy  =  ()=>{
+    if(coin?.market_data?.current_price>=resistance1 && coin?.market_data?.current_price<=resistance2){
+        setbuy("BUY");
+    }
+ }
+
 
   React.useEffect(() => {
     fetchCoinData();
     fetchPrevCoinData();
-    Pivot()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [low, high, close, prevCoin]);
+    buynotbuy();
+        
+  }, [resistance1, buy]);
 
   if (isLoading) return <LinearProgress style={{ backgroundColor: "gold" }} />;
  // console.log(prevCoin)
@@ -275,6 +299,11 @@ const CoinPage = () => {
               &nbsp;&nbsp;
               <Typography variant="h5" style={{ fontFamily: "Montserrat" }}>
                 {support2}
+              </Typography>
+            </span>
+            <span>
+              <Typography>
+                {buy}
               </Typography>
             </span>
           </div>
